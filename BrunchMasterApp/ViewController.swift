e4dd226341117bb5928f676d5f4bc7614f85428d
Zatar <m@zatar.io>
2019-05-17 11:36:08 -0700

    Initial Commit

---
+++ BrunchMasterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrunchMasterApp
+//
+//  Created by Zatar on 5/17/19.
+//  Copyright © 2019 Abstract. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
