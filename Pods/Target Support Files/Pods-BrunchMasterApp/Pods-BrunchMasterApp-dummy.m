60234c91359c9a1c5ed49c6d57845b704e639f01
Zatar <m@zatar.io>
2019-05-17 12:14:20 -0700

    Start of Brunch App with Firebase pods installed

---
+++ Pods/Target Support Files/Pods-BrunchMasterApp/Pods-BrunchMasterApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BrunchMasterApp : NSObject
+@end
+@implementation PodsDummy_Pods_BrunchMasterApp
+@end
---
+++ Pods/Target Support Files/Pods-BrunchMasterAppUITests/Pods-BrunchMasterAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BrunchMasterAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BrunchMasterAppUITests
+@end
