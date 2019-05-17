ae5ed3a7268eae3d72410d9bc676ce5aa321a55c
Zatar <m@zatar.io>
2019-05-17 13:07:05 -0700

    Setup viewcontroller for email login with firebase

--- BrunchMasterApp/AppDelegate.swift
+++ BrunchMasterApp/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+// Custom imports
+import Firebase
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //To send connection to Firebase
+        FirebaseApp.configure()
+        
         return true
     }
 
--- BrunchMasterApp/ViewController.swift
+++ BrunchMasterApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,44 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func loginTapped(_ sender: UIButton) {
+        // Get the defualt auth UI object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            //log the error
+            return
+        
+        }
+        
+        // Set ourselves as the delegate
+        authUI?.delegate = self
+        
+        // Get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // Show it
+        present(authViewController, animated: true, completion: nil)
+        
+    }
+    
+}
+
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        // Check if there was an error
+        if error != nil {
+            //log error
+            return
+        }
+        //If you want to get a users uid from FireStore
+        // authDateResult.user.uid
+        // This allows you to check database side to decide where to send them next. i.g. setup wizard, or account
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+    
 
 }
 
