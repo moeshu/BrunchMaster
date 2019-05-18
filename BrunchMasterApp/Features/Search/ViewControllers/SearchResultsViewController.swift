b91c83928a9a98dac00234dbbf851e0d3613b74b
Zatar <m@zatar.io>
2019-05-18 09:18:10 -0700

    Add structure for Search

---
+++ BrunchMasterApp/Features/Search/ViewControllers/SearchResultsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResultsViewController.swift
+//  BrunchMasterApp
+//
+//  Created by Zatar on 5/18/19.
+//  Copyright © 2019 Abstract. All rights reserved.
+//
+
+import Foundation
---
+++ BrunchMasterApp/Features/Search/ViewControllers/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  BrunchMasterApp
+//
+//  Created by Zatar on 5/18/19.
+//  Copyright © 2019 Abstract. All rights reserved.
+//
+
+import Foundation
